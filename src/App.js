16fb969c79e5b4b672889086ea724cba47097247
Emmm <elemem23@gmail.com>
2024-07-29 00:31:23 +0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is my portfolio</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import { FaFacebookF } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+const Navbar = () => {
+    const socialsStyle = {border: '1px white solid', padding:'8px',height:'30px',width:'30px',marginRight:'10px',borderRadius:'15px'};
+    return (  
+        <div className="navbar">
+            <h1>My portfolio</h1>
+
+            <ul className="links">  
+                 <li>Home</li>
+                <li>Projects</li>
+                <li>Skills</li> 
+                <li style={{ padding:'0',margin:'auto'}}>
+                    <FaFacebookF style={socialsStyle}/>
+                    <FaGithub style={socialsStyle}/>
+                    <FaLinkedinIn style={socialsStyle}/>
+                </li>
+                <li style={{ border:'1px white solid',height:'50px',width:'170px',textAlign:'center',margin:'auto 0 auto 20px',padding:'15px'}}>
+                    Let's Connect
+                </li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default Navbar;
