9d7d7e68ce9a0e0a2e464f364b6eefefa34368a3
Emmm <elemem23@gmail.com>
2024-10-11 23:50:09 +0800

    added a project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,13 @@
 import Carousel from './ProjectsCarousel'
 import "../index.css";  
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+// import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
 import useInterSectionObserver from './useIntersectionObserver';
 import classNames from 'classnames/bind';
 import palengke from '../images/palengke.png'
 import fourtitudeGarage from "../images/fourtitudeGarage.png"
+import bloggaHub from "../images/bloggaHub.png"
 
 const Projects = () => {
   const [ref,isInView,isScrollUp] = useInterSectionObserver();
@@ -15,9 +16,11 @@ const Projects = () => {
         {alt:"Waypoint Navigation",isDeployed: false, img: palengke,  github:"https://github.com/Emmm-07/PALengke-Sofdes-Project", link:"https://reccloud.com/u/2m4r3zj",
           description:"A platform where customers of the public market can find specific products they are looking for. This platform can also provide them with navigation routes with the kiosk as the starting point for new customers who are unfamiliar with the market layout. Users can view which stores sell the product they are looking for. "},
         
-          {alt:"Fourtitude Garage",isDeployed: false, img: fourtitudeGarage, github:"https://github.com/Emmm-07/Fourtitude-Garage", link:"https://reccloud.com/u/onegqtq",
-            description:"A Point of Sale (POS) system for automobile shops that facilitate the functions of buying a car, renting a car, and car servicing with a database system that will store transactions."},
-       
+        {alt:"Fourtitude Garage",isDeployed: false, img: fourtitudeGarage, github:"https://github.com/Emmm-07/Fourtitude-Garage", link:"https://reccloud.com/u/onegqtq",
+          description:"A Point of Sale (POS) system for automobile shops that facilitate the functions of buying a car, renting a car, and car servicing with a database system that will store transactions."},
+        
+        {alt:"Blogga Hub",isDeployed: true, img: bloggaHub, github:"https://github.com/Emmm-07/Blog-App-1", link:"https://my-blog-app-bay.vercel.app/",
+          description:"A simple web blogging app where you can post blogs, edit/delete your blogs, see other user's blogs and leave a heart react if you liked a blog. This has a Json Web Token (JWT) authentication to ensure a secure blogging experience."},
        
     ]
 
